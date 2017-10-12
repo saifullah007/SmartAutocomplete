cc98de5de8b2a60f97a0c20e655d679a79420466
MD Saifullah Khan <=>
2017-10-13 00:44:22 +0530

    default configuration defined

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,35 @@
 var SmartAutocomplete = (function (scope) {
 
     scope.Configuration = function Configuration(options) {
+        var defaults = {
+            data: 'required',
+            url: 'required',
+            maxNoOfContent: 10,
+            paginated: false,
+            paginationQuery: {},
+            getPaginationQuery: function(prevPaginationQuery){
+                return prevPaginationQuery;
+            },
+            listLocation: function(data){
+                return data;
+            },
+            getValue: function(element){
+                return element;
+            },
+
+            onClickEvent: function(e) {},
+            onSelectEvent: function(e) {},
+            onLoadEvent: function(e) {},
+            onKeyEnterEvent: function(e) {},
+            onShowListEvent: function(e) {},
+            onHideListEvent: function(e) {}
+        };
+
+        // var requiredFields = ['data', ''];
+
+        overideDefaultWithOptions(defaults, options);
+        
+        function overideDefaultsWithOption(options){};
     };
 
     return scope;
