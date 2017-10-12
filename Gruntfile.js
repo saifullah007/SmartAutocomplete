2085660578cccb9dfe87acb2d023d93a01ca7621
MD Saifullah Khan <=>
2017-10-13 00:12:31 +0530

    Task created in Gruntfile to concat js in src to  auto-complete.js in dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,30 @@
 module.exports = function (grunt) {
 
+    /**
+     * Add configurations
+     */
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            options: {
+                banner: '/**\n'+
+                ' * <%= pkg.name %>\n'+
+                ' * @Author <%= pkg.author %>\n'+
+                ' */\n',
+
+                separator: '\n\n\n'
+            },
+
+            dist: {
+                src: ['src/configuration.js','src/dataBuilder.js'],
+                dest: 'dist/smart-autocomplete.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat']);
 };
